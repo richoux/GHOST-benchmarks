e717e6609f5d2bc4ed975a51a2110021d70c8444
Florian Richoux <florian.richoux@polytechnique.edu>
2018-04-17 10:36:38 +0200

    Add computers folder for results

--- resource/run_bench.sh
+++ resource/run_bench.sh
@@ -3,4 +3,11 @@
 #$1 = terran/protoss/zerg
 #$2 = number of runs
 
-bin/resource $1 $2 > scripts/bench/$1_$2_$(date +%y-%m-%d_%H-%M).txt
+if [ $# -eq 0 ]; then
+    bin/resource protoss 100 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_protoss_100.txt
+    bin/resource terran 100 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_terran_100.txt
+    bin/resource zerg 100 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_zerg_100.txt
+    exit 0
+else
+    bin/resource $1 $2 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_$1_$2.txt
+fi
