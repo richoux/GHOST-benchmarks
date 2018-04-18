7847157eb24b67a975254c9cde2b4bfecfb8706d
Florian Richoux <florian.richoux@polytechnique.edu>
2018-04-18 13:41:09 +0200

    To make it usable on duran

--- resource/run_bench.sh
+++ resource/run_bench.sh
@@ -3,11 +3,13 @@
 #$1 = terran/protoss/zerg
 #$2 = number of runs
 
+HOST=$(hostname -fs)
+
 if [ $# -eq 0 ]; then
-    bin/resource protoss 100 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_protoss_100.txt
-    bin/resource terran 100 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_terran_100.txt
-    bin/resource zerg 100 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_zerg_100.txt
+    bin/resource protoss 100 > scripts/bench/$HOST/$(date +%y-%m-%d_%H-%M)_protoss_100.txt
+    bin/resource terran 100 > scripts/bench/$HOST/$(date +%y-%m-%d_%H-%M)_terran_100.txt
+    bin/resource zerg 100 > scripts/bench/$HOST/$(date +%y-%m-%d_%H-%M)_zerg_100.txt
     exit 0
 else
-    bin/resource $1 $2 > scripts/bench/$(hostname)/$(date +%y-%m-%d_%H-%M)_$1_$2.txt
+    bin/resource $1 $2 > scripts/bench/$HOST/$(date +%y-%m-%d_%H-%M)_$1_$2.txt
 fi
