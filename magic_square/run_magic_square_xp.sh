6505e4310b0b450fbd3d03d12a48ecbbfa241ed2
Florian Richoux <florian.richoux@polytechnique.edu>
2021-11-10 16:45:24 +0900

    Changes with AS parameter tunings

--- magic_square/run_magic_square_xp.sh
+++ magic_square/run_magic_square_xp.sh
@@ -6,7 +6,7 @@ path_results=results/"$date"/"$host_name"
 mkdir -p "$path_results"
 
 for i in {1..100}; do
-    ./bin/magic_square 20 1 8 >> ./"$path_results"/magic_square_20_8cores.txt
-    ./bin/magic_square 30 1 8 >> ./"$path_results"/magic_square_30_8cores.txt
-    ./bin/magic_square 40 1 8 >> ./"$path_results"/magic_square_40_8cores.txt
+    ./bin/magic_square 20 1 8 >> ./"$path_results"/magic_square_20_astuning_8cores.txt
+    ./bin/magic_square 30 1 8 >> ./"$path_results"/magic_square_30_astuning_8cores.txt
+    ./bin/magic_square 40 1 8 >> ./"$path_results"/magic_square_40_astuning_8cores.txt
 done
--- magic_square/src/magic_square.cpp
+++ magic_square/src/magic_square.cpp
@@ -112,6 +112,14 @@ int main( int argc, char **argv )
 	Options options;
 	options.print = printer;
 
+	// Parameters tuning from Diaz et al.
+	options.percent_chance_escape_plateau = 6;
+	options.tabu_time_selected = 0;
+	options.tabu_time_local_min = 5;
+	options.reset_threshold = order;
+	options.restart_threshold = 0;
+	options.number_variables_to_reset = std::max( 1, order / 10 );
+	
 	if( order == 8 )
 		options.custom_starting_point = true;
 
