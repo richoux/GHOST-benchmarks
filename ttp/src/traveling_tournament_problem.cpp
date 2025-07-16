9899c993ae49f52b788b428e070b4e8097e4c591
Florian Richoux <florian@richoux.fr>
2025-07-17 07:20:45 +0900

    Tweaking parameters for TTP

--- ttp/src/traveling_tournament_problem.cpp
+++ ttp/src/traveling_tournament_problem.cpp
@@ -55,6 +55,7 @@ int main( int argc, char **argv )
 	options.print = printer;
 	options.number_start_samplings = number_teams * number_teams;
 	options.enable_optimization_guidance = false;
+	options.max_moves_in_opt_space = 2; //TODO: to tweak
 	if( parallel )
 		options.parallel_runs = true;
 	
